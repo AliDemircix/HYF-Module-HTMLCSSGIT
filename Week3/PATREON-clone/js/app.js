227f7b0c914c86edd1aad968cbeee71395b4bd74
AliDemircix <alidemircix@gmail.com>
2021-11-19 00:20:57 +0100

    changeImg javascript eventlistener added

--- Week3/PATREON-clone/js/app.js
+++ Week3/PATREON-clone/js/app.js
@@ -1,1 +1,43 @@
-document.querySelector("p").style.color=`white`;
+const leftImg = document.querySelector(".dump-img");
+const pList = document.querySelectorAll(".p-list li");
+
+const changeImg = (e)=>{
+  const liValue = e.target.textContent;
+  // console.log(liValue)
+  switch(liValue){
+    case 'Podcasters':
+      leftImg.src="./img/podcasters.png"
+      break;
+    case 'Video Creators':
+      leftImg.src="./img/videocreators.png"  
+      break;
+    case 'Musicians':
+      leftImg.src="./img/musicians.png"
+    break;
+    case 'Visual Artists':
+      leftImg.src="./img/visualartists.png"
+    break;
+    case 'Communities':
+      leftImg.src="./img/communities.png"
+    break;
+    case 'Writers & Journalists':
+      leftImg.src="./img/writers.png"
+    break;
+    case 'Gaming Creators':
+      leftImg.src="./img/gamingcreators.png"
+    break;
+    case 'Nonprofits':
+      leftImg.src="./img/nonprofit.png"
+    break;
+    case 'Tutorials and Education':
+      leftImg.src="./img/tutorials.png" 
+    break;
+    case 'Creators-of-all-kinds':
+      leftImg.src="./img/creatorsofallkinds.png"  
+    break;
+  }
+}
+  pList.forEach(list=> {
+    list.addEventListener("mouseover",changeImg);
+  })
+
